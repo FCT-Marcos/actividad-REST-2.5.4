d69ac2f5c0e9fd1e9ccd3e74e2ef3dbc7937ec8c
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-01 15:47:33 +0100

    First commit

---
+++ src/test/java/com/marcos/actividad254/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.actividad254;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
