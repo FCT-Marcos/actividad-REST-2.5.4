8eec100dc9243985af3b448d41233cac5402d93a
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-02 22:31:58 +0100

    Creation securityConfig

---
+++ src/main/java/com/marcos/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.marcos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.marcos.config.filter.JwtTokenValidator;
+import com.marcos.util.JwtUtils;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtUtils jwtUtils) throws Exception {
+		return httpSecurity.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(http -> {
+
+					// Publico
+					http.requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll();
+					http.requestMatchers(HttpMethod.GET, "/api/v1/page/public").permitAll();
+					
+					// Protegido
+					http.requestMatchers(HttpMethod.GET, "/api/v1/page/protected").authenticated();
+
+					// Los enpoints no definidos denegar el acceso
+					http.anyRequest().denyAll();
+				})
+				.addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class)
+				.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager getUserDetailsManager() {
+		return new InMemoryUserDetailsManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
